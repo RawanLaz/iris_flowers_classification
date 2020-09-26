8a6f5f51767dd5a6188d291c0d78995652d0ea8b
Rawan Lazkani <roro.naruto@gmail.com>
2020-09-26 18:59:50 +0200

    training code added

--- data_visualization.py
+++ data_visualization.py
@@ -1,5 +1,6 @@
 from pandas import read_csv
 from matplotlib import pyplot
+from pandas.plotting import scatter_matrix
 
 # Load dataset
 url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
@@ -8,4 +9,10 @@ dataset = read_csv(url, names=names)
 
 # box and whisker plots
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+# histograms
+dataset.hist()
+pyplot.show()
+# scatter plot matrix
+scatter_matrix(dataset)
 pyplot.show()
